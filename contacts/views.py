1e8190bd4c5002ef2987acbf027867bc4c440d91
dmurawski018 <dmurawski018@gmail.com>
2025-04-24 20:00:25 +0200

    displaying feedback, indicators

--- contacts/views.py
+++ contacts/views.py
@@ -12,6 +12,9 @@ def index(request):
 
 @login_required
 def search_contacts(request):
+    import time
+
+    time.sleep(2)
     query = request.GET.get("search", "")
 
     contacts = request.user.contacts.filter(
