cc7a56156f5c72d31dda17e4f88c62c8fcd769cd
dmurawski018 <dmurawski018@gmail.com>
2025-04-24 15:18:04 +0200

    HTMX active search

--- contacts/views.py
+++ contacts/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models import Q
 from django.shortcuts import render
 
 
@@ -7,3 +8,14 @@ def index(request):
     contacts = request.user.contacts.all().order_by("-created_at")
     context = {"contacts": contacts}
     return render(request, "contacts.html", context)
+
+
+@login_required
+def search_contacts(request):
+    query = request.GET.get("search", "")
+
+    contacts = request.user.contacts.filter(
+        Q(name__icontains=query) | Q(email__icontains=query)
+    )
+
+    return render(request, "partials/contacts-list.html", {"contacts": contacts})
